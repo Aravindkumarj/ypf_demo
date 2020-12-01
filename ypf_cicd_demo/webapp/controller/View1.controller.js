3a6efb51c0f22548c748cb44f64fb441c1c4113f
aravindkumarj <aravindkumarj@gmail.com>
2020-12-01 17:41:40 +0000

    initial commit

---
+++ ypf_cicd_demo/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ypf.ypf_cicd_demo.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ ypf_cicd_demo/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ypf/ypf_cicd_demo/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
